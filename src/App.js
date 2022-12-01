171755953cddfa6ce3ffe7b0c0a464165e2d5c69
Superstar27 <91425528+Superstar27@users.noreply.github.com>
2022-12-01 21:53:24 +0530

    new

--- src/App.js
+++ src/App.js
@@ -4,21 +4,72 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+ 
+    <div className='App-Container'>
+    <h3 className='Header'>
+      Australia's most secure & trusted garage door brand
+    </h3>
+    <div className='Detail'>
+      We know that behind your garage door are the things that matters to you most. The things that makes your house a home.
+      Most garage doors just close but a B&D garage door can be lock.The things that  an home.
     </div>
+  <div className="flexDisplay" >
+    <div className='Detail-info'>
+      <div className='Info-icon'>
+        <div className='Icon-text'>
+        <div className='Icon'>
+          <img  className="Image"src='guarantee.png' alt="gurantee"/>
+        </div>
+        <div className='Info'>
+          10 Year Warranty
+        </div>
+        </div>
+      <div className='objectives'>
+            All our garage doors component and accessories -other than batteries,bulbs or fuses incude one year warranty
+ 
+      </div>
+      </div>
+ 
+    </div>
+    <div className='Detail-info'>
+      <div className='Info-icon'>
+        <div className='Icon-text'>
+        <div className='Icon'>
+          <img  className="Image"src='six.png' alt="gurantee"/>
+        </div>
+        <div className='Info'>
+          10 Year Warranty
+        </div>
+        </div>
+      <div className='objectives'>
+            All our garage doors component and accessories -other than batteries,bulbs or fuses incude one year warranty
+ 
+      </div>
+      </div>
+ 
+    </div>
+    <div className='Detail-info'>
+      <div className='Info-icon'>
+        <div className='Icon-text'>
+        <div className='Icon'>
+          <img  className="Image"src='aus.png' alt="gurantee"/>
+        </div>
+        <div className='Info'>
+          10 Year Warranty
+        </div>
+        </div>
+      <div className='objectives'>
+            All our garage doors component and accessories -other than batteries,bulbs or fuses incude one year warranty
+ 
+      </div>
+      </div>
+ 
+    </div>
+    </div>
+  </div>
+  </div>
+
   );
 }
 
